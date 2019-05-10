16ca90f6019741368774e6a51a307a77acceaa98
AdamWil_ <adam6457@gmail.com>
2019-05-10 10:42:09 -0600

    Added threaded IEX api, CSV merge, and a LSTM. LSTM needs testing and tunning

---
+++ IEX/merge_csvs.py
@@ -0,0 +1,12 @@
+
+import os
+import glob
+import pandas as pd
+os.chdir("tmp/")
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+os.chdir("../")
+combined_csv.to_csv("combined_csv.csv", index=False, encoding='utf-8-sig')
+
+print('Fin')
